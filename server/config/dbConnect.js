554409cadc5b6074d857e80cde552ce5ab2b1cd7
TranThang <trannthang2212@gmail.com>
2023-02-10 12:31:56 +0700

    #2 Handle error

---
+++ server/config/dbConnect.js
@@ -0,0 +1,16 @@
+const { default: mongoose } = require("mongoose");
+mongoose.set("strictQuery", true);
+
+const dbConnect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    if (conn.connection.readyState === 1)
+      console.log("DB connection is successfully");
+    else console.log("DB Connecting");
+  } catch (error) {
+    console.log("DB Connection is failed");
+    throw new Error("");
+  }
+};
+
+module.exports = dbConnect;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 require("dotenv").config();
+const dbConnect = require("./config/dbConnect");
+const initRoutes = require("./routes/index");
 
 const app = express();
 const port = process.env.PORT || 8888;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/", (req, res) => {
-  res.send("Server on");
-});
+dbConnect();
+initRoutes(app);
 
 app.listen(port, () => {
   console.log("Server running on the port " + port);
