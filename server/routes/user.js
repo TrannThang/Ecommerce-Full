b569d164a33990b48bf29c2ee45cc2a5255af571
TranThang <trannthang2212@gmail.com>
2023-02-22 12:29:52 +0700

    2

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
 const ctrls = require("../controllers/user");
+const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
 
 router.post("/register", ctrls.register);
+router.post("/login", ctrls.login);
+router.get("/current", verifyAccessToken, ctrls.getCurrent);
+router.post("/refreshtoken", ctrls.refreshAccessToken);
+router.get("/logout", ctrls.logout);
+router.get("/forgotpassword", ctrls.forgotPassword);
+router.put("/resetpassword", ctrls.resetPassWord);
+router.get("/", [verifyAccessToken, isAdmin], ctrls.getUsers);
+router.delete("/", [verifyAccessToken, isAdmin], ctrls.deleteUser);
+router.put("/current", [verifyAccessToken], ctrls.updateUser);
+router.put("/:uid", [verifyAccessToken, isAdmin], ctrls.updateUserByAdmin);
+
 //eee
 module.exports = router;
+
+// Create + put => body
+// Get + Delete - query ?????=>query
